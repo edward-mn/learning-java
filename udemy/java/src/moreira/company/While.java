f69694488eddbd43159f46f1f1cf17cd26c854c4
Edward Moreira <edward.moreira@yahoo.com>
2021-09-16 12:07:56 -0300

    Java POO
    Others :heavy_check_mark:

---
+++ udemy/java/src/moreira/company/While.java
@@ -0,0 +1,20 @@
+package moreira.company;
+
+import java.util.Scanner;
+
+public class While {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();;
+
+    int total = 0;
+    while (number != 0){
+      total += number;
+      number = sc.nextInt();
+      System.out.println("Para saix digite 0");
+    }
+
+    System.out.println("O total foi = " + total);
+    sc.close();
+  }
+}
