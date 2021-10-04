d54ec60ae9ef9191161ca2023dd8f10f25d0bf13
Edward Moreira <edward.moreira@yahoo.com>
2021-10-04 18:00:45 -0300

    Java POO
    Static Members - pt3 (DONE) :heavy_check_mark:

--- udemy/java/src/util/Calculator.java
+++ udemy/java/src/util/Calculator.java
@@ -1,13 +1,13 @@
 package util;
 
 public class Calculator {
-  public final double PI = 3.14159; // Final = Const - Static = Não necessita de um obj para executar
+  public static final double PI = 3.14159; // Final = Const - Static = Não necessita de um obj para executar
 
-  public double circumference(double radius){
+  public static double circumference(double radius){
     return 2.0 * PI * radius;
   }
 
-  public double volume(double radius){
+  public static double volume(double radius){
     return 4.0 * PI * Math.pow(radius, 3) / 3.0;
   }
 }
