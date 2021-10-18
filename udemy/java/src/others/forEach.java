7a6223e4908ff6ec1e095c52f1f05e3fa01d707e
Edward Moreira <edward.moreira@yahoo.com>
2021-10-18 17:41:01 -0300

    Java POO
    For Each :heavy_check_mark:

---
+++ udemy/java/src/others/forEach.java
@@ -0,0 +1,11 @@
+package others;
+
+public class forEach {
+  public static void main(String[] args) {
+    String[] names = new String[] {"Edward", "Maria", "Fábio", "Luiz"};
+
+    for(String name: names){
+      System.out.println(name);
+    }
+  }
+}
