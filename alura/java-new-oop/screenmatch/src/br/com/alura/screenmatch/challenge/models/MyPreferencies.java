da171541b431640f4487c4e47008ecc734e96f49
Edward Moreira <edward.moreira@yahoo.com>
2023-05-19 18:22:44 -0300

    Finish curse
    New Java OOP :new:

--- alura/java-new-oop/screenmatch/src/br/com/alura/screenmatch/challenge/models/MyPreferencies.java
+++ alura/java-new-oop/screenmatch/src/br/com/alura/screenmatch/challenge/models/MyPreferencies.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.challenge.models;
 public class MyPreferencies {
     public void includes(Audio audio){
         String message = audio.getTitle();
+
         if (audio.getClassifications()>= 9){
             System.out.println(message += " - it's a sucessfull!");
         } else {
